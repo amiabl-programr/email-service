efa263f6e5c14a08f95ba222b7caf58b73938e7e
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-13 08:12:40 +0100

    chore: change function name to snakecase

--- src/services/template.service.ts
+++ src/services/template.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export async function fetchTemplate(template_type: string, language: string) {
+export async function fetch_template(template_type: string, language: string) {
   const response = await axios.get(
     `${process.env.TEMPLATE_SERVICE_URL}/templates/latest`,
     { params: { type: template_type, language } }
