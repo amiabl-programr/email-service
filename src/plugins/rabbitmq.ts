375e9b0eaa610e7a02162fcb5b1b969742550784
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-13 08:15:51 +0100

    feat(rabbitmq): initialize RabbitMQ connection and queue setup

---
+++ src/plugins/rabbitmq.ts
@@ -0,0 +1,16 @@
+import amqplib from 'amqplib';
+
+export async function connectRabbitMQ() {
+  const connection = await amqplib.connect(process.env.RABBITMQ_URL!);
+  const channel = await connection.createChannel();
+
+  const exchange = 'notifications.direct';
+  await channel.assertExchange(exchange, 'direct', { durable: true });
+
+  const queue = 'email.queue';
+  await channel.assertQueue(queue, { durable: true });
+  await channel.bindQueue(queue, exchange, 'email');
+
+  console.log('✅ Connected to RabbitMQ and email.queue is ready');
+  return channel;
+}
