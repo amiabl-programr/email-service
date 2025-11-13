6eb94fd97c52a42890feec9210561d4c477c7835
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-13 07:50:06 +0100

    feat: create fetch_template function

---
+++ src/services/template.service.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export async function fetchTemplate(template_type: string, language: string) {
+  const response = await axios.get(
+    `${process.env.TEMPLATE_SERVICE_URL}/templates/latest`,
+    { params: { type: template_type, language } }
+  );
+
+  return response.data.data; // expect { subject, body }
+}
